068f9b6fe09b5933a49ad571643af30bb2222bfc
israelizzy123 <31420706+israelizzy123@users.noreply.github.com>
2019-03-04 13:53:28 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function rotateImage(el){
 	el.classList.toggle("rotate");
+	el.classList.add("rotate");
+}
+function rotateImage2(el){
+	el.classList.toggle("rotate2");
+	el.classList.add("rotate2");
+}
+function rotateImage3(el){
+	el.classList.toggle("rotate3");
+	el.classList.add("rotate3");
 }
