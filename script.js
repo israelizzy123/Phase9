ba405bb08165574dfac71b3baebc010abdd8ed76
israelizzy123 <31420706+israelizzy123@users.noreply.github.com>
2019-03-12 08:27:10 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+a = document.getElementById("1");
+b = document.getElementById("2");
+c = document.getElementById("3");
+d = document.getElementById("4");
+e = document.getElementById("5");
+f = document.getElementById("6");
+g = document.getElementById("7");
+h = document.getElementById("8");
+i = document.getElementById("9");
+
+
 function rotateImage(el){
 	el.classList.toggle("rotate");
 	el.classList.add("rotate");
@@ -9,4 +20,26 @@ function rotateImage2(el){
 function rotateImage3(el){
 	el.classList.toggle("rotate3");
 	el.classList.add("rotate3");
+}
+function checkImage(el){
+	if(a.classList.contains("rotate") && b.classList.contains("rotate2") && c.classList.contains("rotate3") && d.classList.contains("rotate")&& e.classList.contains("rotate2")&& f.classList.contains("rotate3")&& g.classList.contains("rotate")&& h.classList.contains("rotate2")&& i.classList.contains("rotate3")){
+		console.log("You Win!");
+		a.classList.add("opaque");
+		b.classList.add("opaque");
+		c.classList.add("opaque");
+		d.classList.add("opaque");
+		e.classList.add("opaque");
+		f.classList.add("opaque");
+		g.classList.add("opaque");
+		h.classList.add("opaque");
+		i.classList.add("opaque");
+		document.getElementById("key").classList.remove("hide");
+	}
+	else {
+		window.alert("awww");
+	}
+
+}
+function resetWeb(){
+	window.location.reload();
 }
